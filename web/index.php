a2457353b3f8fcbe8d933e00e5526b01f21647d9
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-06-18 00:34:51 +0200

    Remove all useless checks

--- web/index.php
+++ web/index.php
@@ -7,22 +7,6 @@ use Symfony\Component\Debug\Debug;
 require __DIR__.'/../vendor/autoload.php';
 
 if (getenv('APP_DEBUG')) {
-    // This check prevents access to debug front controllers that are deployed by accident to production servers.
-    // Feel free to remove this, extend it, or make something more sophisticated.
-    $whitelistedIps = ['127.0.0.1', '::1'];
-
-    // Allow access from the host
-    if ($dockerBridgeIp = getenv('DOCKER_BRIDGE_IP')) {
-        $whitelistedIps[] = $dockerBridgeIp;
-    }
-    if (isset($_SERVER['HTTP_CLIENT_IP'])
-        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-        || !(in_array(@$_SERVER['REMOTE_ADDR'], $whitelistedIps) || php_sapi_name() === 'cli-server')
-    ) {
-        header('HTTP/1.0 403 Forbidden');
-        exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-    }
-
     Debug::enable();
 }
 
